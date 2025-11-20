ccec04ba09c218a79defde18313e4ec5f77793b3
asimhusain-ai <studyboyasim01@gmail.com>
2025-11-20 18:15:00 +0530

    a

--- services/api_service.py
+++ services/api_service.py
@@ -5,7 +5,6 @@ import time
 import random
 import re
 import json
-from app import db
 from textblob import TextBlob
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
